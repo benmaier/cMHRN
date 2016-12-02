778d673bbc9003a7bd662fc10692af4f8f14b195
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-12-02 12:59:49 +0100

    added matlab test

---
+++ sandbox/matlab_test.m
@@ -0,0 +1,18 @@
+clear all;
+close all;
+
+B = 4;
+L = 3;
+k = 3;
+xi = 0.3;
+use_giant_component = false;
+delete_nodes_non_giant = false;
+random_seed = 3984576;
+
+edge_list = fast_mhrn(B,L,k,xi,use_giant_component,delete_nodes_non_giant,random_seed);
+
+rows = edge_list(:,1) + 1;
+cols = edge_list(:,2) + 1;
+
+G = graph(rows,cols);
+plot(G);
